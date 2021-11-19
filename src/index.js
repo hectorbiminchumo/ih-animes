f4695c541872b8a7885feb0f1ad99ca68137c4b5
Hector Biminchumo <hectorbiminchumo@gmail.com>
2021-11-19 11:27:41 -0500

    db

---
+++ src/index.js
@@ -0,0 +1,28 @@
+// Imports
+const express = require('express')
+const app = express()
+
+require('dotenv').config()
+
+const path = require('path')
+
+// Middlewares
+// Es una funcion que se ejecuta despues de recibir una peticion y antes de dar una respuesta
+// Trabajar con archivos estaticos
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Configuraciones
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
+
+// Routes
+// Home
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+
+// Server
+app.listen(process.env.PORT, () => {
+    console.log(`Server on port: http://localhost:${process.env.PORT}`)
+})
